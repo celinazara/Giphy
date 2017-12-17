e913ca7c3455d0f9a86597c90342f4338513ca4c
Celina <celina.zaragoza@gmail.com>
2017-12-16 19:53:52 -0600

    logo

--- assets/giphy.js
+++ assets/giphy.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 //Array for searched topics to be added
 var topics = [];
 
-	//Function with AJAX call to GIPHY; Q parameterc for API link set to search term, limit 10 results
-  //Create div with respective still and animate image sources with "data-state", "data-still" and "data-animate" attributes
+	
+  
  	function displayNetflixShow() {
 
 	var x = $(this).data("search");
 	console.log(x);
 
-	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8yWkpYGD5UO0aWNUjEMvx2I7kJAXReUu&q=Netflix&limit=10&offset=0&rating=PG-13&lang=en";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8yWkpYGD5UO0aWNUjEMvx2I7kJAXReUu&q=netflix&limit=10&offset=0&lang=en";
 	console.log(queryURL);
 
 	$.ajax({
@@ -51,7 +51,7 @@ var topics = [];
         displayButtons();
       });
 
-  //Function iterates through topics array to display button with array values in "myButtons" section of HTML
+ 
 	function displayButtons() {
     $("#myButtons").empty();
     for (var i = 0; i < topics.length; i++) {
@@ -66,13 +66,13 @@ var topics = [];
 
   displayButtons();
 
-  //Click event on button with id of "show" executes displayNetflixShow function
+  
   $(document).on("click", "#show", displayNetflixShow);
 
-  //Click event on gifs with class of "netflixGiphy" executes pausePlayGifs function
+  
   $(document).on("click", ".netflixGiphy", pausePlayGifs);
 
-  //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
+  
   function pausePlayGifs() {
   	 var state = $(this).attr("data-state");
       if (state === "still") {
